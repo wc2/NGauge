33e055cda0e3759b65e0eb895af7959171a64414
William Cowell <recruit-me@williamcowell.com>
2016-06-03 16:21:47 +0100

    Added missing test file

---
+++ src/Tests/Gauge.NCrunch.Runner.Tests/BridgeTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Gauge.NCrunch.Runner.Tests
+{
+    public sealed class BridgeTests
+    {
+        [Fact]
+        public void CreateScenarioRunner_ReturnsScenarioRunner()
+        {
+            var scenarioRunner = Bridge.CreateScenarioRunner();
+
+            scenarioRunner
+                .Should()
+                .BeOfType<ScenarioRunner>();
+        }
+    }
+}
