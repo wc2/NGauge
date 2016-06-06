ecae4bc1a5d9e754bea041f629af168debbeebd5
William Cowell <wonkidonk@outlook.com>
2016-06-06 15:51:52 +0100

    #2 Deprecated support for Concepts
    Removed IStepFactory.Create(ProtoConcept)

--- src/Libraries/Gauge.NCrunch.Specs.Reader/Factories/IStepFactory.cs
+++ src/Libraries/Gauge.NCrunch.Specs.Reader/Factories/IStepFactory.cs
@@ -4,7 +4,6 @@ namespace Gauge.NCrunch.Specs.Reader.Factories
 {
     public interface IStepFactory
     {
-        IStep Create(ProtoConcept protoConcept);
         IStep Create(ProtoStep protoStep);
     }
 }
--- src/Libraries/Gauge.NCrunch.Specs.Reader/Factories/StepFactory.cs
+++ src/Libraries/Gauge.NCrunch.Specs.Reader/Factories/StepFactory.cs
@@ -4,11 +4,6 @@ namespace Gauge.NCrunch.Specs.Reader.Factories
 {
     internal sealed class StepFactory : IStepFactory
     {
-        IStep IStepFactory.Create(ProtoConcept protoConcept)
-        {
-            throw new System.NotImplementedException();
-        }
-
         IStep IStepFactory.Create(ProtoStep protoStep)
         {
             throw new System.NotImplementedException();
