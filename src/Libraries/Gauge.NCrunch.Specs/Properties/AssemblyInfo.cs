c0e93ca4e5e455cffedbbe401d018b45e60859ce
William Cowell <wonkidonk@outlook.com>
2016-06-06 11:16:11 +0100

    #2 Defined Specs Project
    Defined a project to parse Specifications. A model of the parsed
    Specifications will be used to write a bridge between the specs and
    NCrunch.

---
+++ src/Libraries/Gauge.NCrunch.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Gauge.NCrunch.Specs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: InternalsVisibleTo("Gauge.NCrunch.Specs.Tests")]
