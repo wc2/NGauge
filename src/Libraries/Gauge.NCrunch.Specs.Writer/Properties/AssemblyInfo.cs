2d4178572a120997da6270c8def494dddafe07ae
William Cowell <wonkidonk@outlook.com>
2016-06-08 12:57:13 +0100

    #5 Create Writer project
    Defined Gauge.NCrunch.Specs.Writer to define the interfaces required for
    writing Specifications to an NCrunch-bridged format.

---
+++ src/Libraries/Gauge.NCrunch.Specs.Writer/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("Gauge.NCrunch.Specs.Writer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
