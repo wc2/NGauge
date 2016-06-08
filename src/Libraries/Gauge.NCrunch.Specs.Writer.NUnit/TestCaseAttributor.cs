f727b3fb4f8a542a23fd036259d37cfdf47f879a
William Cowell <wonkidonk@outlook.com>
2016-06-08 14:55:11 +0100

    #6 Began NUnit implementation

---
+++ src/Libraries/Gauge.NCrunch.Specs.Writer.NUnit/TestCaseAttributor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gauge.NCrunch.Specs.Writer.NUnit
+{
+    public sealed class TestCaseAttributor : IGetInvariantTestAttributor
+    {
+        Type IGetInvariantTestAttributor.GetAttribute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Tests/Gauge.NCrunch.Specs.Writer.NUnit.Tests/TestCaseAttributorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauge.NCrunch.Specs.Writer.NUnit.Tests
+{
+    class TestCaseAttributorTests
+    {
+    }
+}
