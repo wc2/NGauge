63b27419a3b685880b06c445e99b4bacd2e64f1c
William Cowell <recruit-me@williamcowell.com>
2016-06-09 10:56:25 +0100

    #3 Defined SpecificationWriter Function
    Definited the functions that the SpecificationWriter should complete.

--- src/Tests/NGauge.Specs.Writer.Tests/SpecificationsWriterTests.cs
+++ src/Tests/NGauge.Specs.Writer.Tests/SpecificationsWriterTests.cs
@@ -1,7 +1,21 @@
-﻿namespace NGauge.Specs.Writer.Tests
+﻿using System.Threading.Tasks;
+
+namespace NGauge.Specs.Writer.Tests
 {
     public sealed class SpecificationsWriterTests
     {
-        
+        public void ctor_GeneratedCodeNamingServiceRequired() { }
+        public void ctor_SpecificationCodeGeneratorServiceRequired() { }
+        public void ctor_FileDeletionServiceRequired() { }
+        public void ctor_CodeSavingServiceRequired() { }
+        public async Task WriteSpecificationsAsync_SpecificationsRequired() { }
+
+        public async Task WriteSpecificationsAsync_ProjectPathRequired() { }
+
+        public async Task WriteSpecificationsAsync_DeletesPreviouslyGeneratedCodeFile() { }
+        public async Task WriteSpecificationsAsync_GeneratesSpecificationCode() { }
+        public async Task WriteSpecificationsAsync_SavesGeneratedSpecificationCode() { }
+
+        public async Task WriteSpecificationsAsync_ReturnsExpectedGeneratedCodeFileName() { }
     }
 }
