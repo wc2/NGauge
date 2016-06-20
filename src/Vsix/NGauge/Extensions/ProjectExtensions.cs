44712c472ac1378cfb989d31b7a74f4b6fab82a5
William Cowell <WonkiDonk@outlook.com>
2016-06-20 15:31:14 +0100

    Updated adding and removing of generated code files, as it was keeping older references around :-/

--- src/Vsix/NGauge/Extensions/ProjectExtensions.cs
+++ src/Vsix/NGauge/Extensions/ProjectExtensions.cs
@@ -1,4 +1,7 @@
-﻿using EnvDTE;
+﻿using System;
+using System.IO;
+using System.Linq;
+using EnvDTE;
 
 namespace NGauge.Extensions
 {
@@ -6,8 +9,35 @@ namespace NGauge.Extensions
     {
         internal static void EnsureFolderIsReferenced(this Project project, string folderName)
         {
-            project.ProjectItems.AddFromDirectory(folderName);
+            project.RemoveDirectory(folderName);
+            project.AddDirectory(folderName);
             project.Save();
+        }
+
+        private static void RemoveDirectory(this Project project, string folderName)
+        {
+            var existingItemsInFolder = project.ProjectItems
+                .Cast<ProjectItem>()
+                .Where(projectItem => IsWithinFolder(projectItem, folderName));
+
+            foreach (var projectItem in existingItemsInFolder)
+            {
+                projectItem.Remove();
+            }
+        }
+
+        private static void AddDirectory(this Project project, string folderName)
+        {
+            foreach (var codeGeneratedFile in Directory.EnumerateFiles(folderName, "*.cs"))
+            {
+                project.ProjectItems.AddFromFile(codeGeneratedFile);
+            }
+        }
+
+        private static bool IsWithinFolder(ProjectItem projectItem, string folderName)
+        {
+            return Path.GetDirectoryName(projectItem.FileNames[0])
+                .Equals(folderName, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
