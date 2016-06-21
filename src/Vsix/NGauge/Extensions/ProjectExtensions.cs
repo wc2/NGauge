97d5b4cac643810807a16d86b6c3c68a3b9e9bb6
William Cowell <WonkiDonk@outlook.com>
2016-06-21 08:11:27 +0100

    Updated plug-in to ensure xunit and NGauge.Runner packages are installed in target project when tests are updated.

--- src/Vsix/NGauge/Extensions/ProjectExtensions.cs
+++ src/Vsix/NGauge/Extensions/ProjectExtensions.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Linq;
 using EnvDTE;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using NuGet.VisualStudio;
 
 namespace NGauge.Extensions
 {
@@ -16,12 +19,12 @@ namespace NGauge.Extensions
 
         internal static void EnsureRunnerIsReferenced(this Project project)
         {
-            
+            project.EnsureNugetPackageIsReferenced("NGauge.Runner");
         }
 
         internal static void EnsureXUnitIsReferenced(this Project project)
         {
-            
+            project.EnsureNugetPackageIsReferenced("xunit");
         }
 
         private static void RemoveDirectory(this Project project, string folderName)
@@ -48,6 +51,18 @@ namespace NGauge.Extensions
         {
             return Path.GetDirectoryName(projectItem.FileNames[0])
                 .Equals(folderName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private static void EnsureNugetPackageIsReferenced(this Project project, string packageName)
+        {
+            var componentModel = (IComponentModel) Package.GetGlobalService(typeof(SComponentModel));
+            var installerServices = componentModel.GetService<IVsPackageInstallerServices>();
+
+            if (!installerServices.IsPackageInstalled(project, packageName))
+            {
+                var installer = componentModel.GetService<IVsPackageInstaller>();
+                installer.InstallPackage("All", project, packageName, default(Version), false);
+            }
         }
     }
 }
